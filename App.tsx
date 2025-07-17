6d2282d50d1bce78665918bf5e1df7dbc6e9b803
root <root@localhost>
2025-07-17 18:41:35 +0000

    .

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
+import SignUp from '.src/screens/SignUpScreen.tsx';
 
 const Stack = createNativeStackNavigator();
 
@@ -21,6 +22,7 @@ const AppNavigator = () => {
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="SignUp" component={SignUp} />
         {/* Puedes agregar Login y SignUp aquí si los usas */}
       </Stack.Navigator>
     </NavigationContainer>
